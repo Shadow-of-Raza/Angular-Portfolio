effe1e0c9de3115bb38afaeb87ddb806cd148975
Guest Guru <guestguru00@gmail.com>
2025-05-05 03:31:43 +0530

    “message….”

--- src/app/component/resume/resume.component.ts
+++ src/app/component/resume/resume.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: [
+              './resume.css/resume.component.css',
+              './resume.css/resume.component.responsive.css',
+            ],
+ 
 })
 export class ResumeComponent {
 
